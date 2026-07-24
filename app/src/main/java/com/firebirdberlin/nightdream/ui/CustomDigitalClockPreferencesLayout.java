00dc5cb56ef27b4b1cfa09905a33fe594d68f968
Vijayakumar G <gvijayakumar.ganesh@gmail.com>
2026-07-24 14:12:52 +0530

    Removed unused fonts

--- app/src/main/java/com/firebirdberlin/nightdream/ui/CustomDigitalClockPreferencesLayout.java
+++ app/src/main/java/com/firebirdberlin/nightdream/ui/CustomDigitalClockPreferencesLayout.java
@@ -167,11 +167,7 @@ public class CustomDigitalClockPreferencesLayout extends LinearLayout implements
             ManageFontsDialogFragment dialog = new ManageFontsDialogFragment();
             dialog.setIsPurchased(isPurchased);
             dialog.setSelectedUri(settings.getFontUri(layoutId));
-            dialog.setDefaultFonts(
-                    "roboto_regular.ttf", "roboto_light.ttf",
-                    "roboto_thin.ttf", "7_segment_digital.ttf", "dseg14classic.ttf",
-                    "dancingscript_regular.ttf"
-            );
+            dialog.setDefaultFonts("7_segment_digital.ttf");
             dialog.setOnFontSelectedListener(new ManageFontsDialogFragment.ManageFontsDialogListener() {
                 @Override
                 public void onFontSelected(Uri uri, String name) {
